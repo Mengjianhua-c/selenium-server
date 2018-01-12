2a515a3b7904b44bd5541bd12dfaf7137feccc9d
meng <1711621009@qq.com>
2018-01-12 20:38:03 +0800

    startapp poll api

--- taskApi/urls.py
+++ taskApi/urls.py
@@ -5,10 +5,12 @@ AUTHOR:　HEHAHUTU
 from django.conf.urls import include, url
 from rest_framework import routers
 from taskApi import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 router = routers.DefaultRouter()
 router.register(r'task', views.TaskViewSet)
 
 urlpatterns = [
-    url(r'^', include(router.urls))
+    url(r'^', include(router.urls)),
+
 ]
--- taskApi/views.py
+++ taskApi/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from taskApi.models.task import TaskSerializers, Task
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 # Create your views here.
